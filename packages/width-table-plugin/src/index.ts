2453b9ef5169bd8b67178dd36be1cb13788986ee
Kevin Tun <kevin.tun@northwestern.edu>
2023-02-21 08:36:47 -0600

    chore(packages/width-table-plugin): export INSERT_WIDTH_TABLE_COMMAND

--- packages/width-table-plugin/src/index.ts
+++ packages/width-table-plugin/src/index.ts
@@ -1,2 +1,5 @@
 export { default as WidthTableNode } from "./WidthTableNode"
-export { default as WidthTablePlugin } from "./WidthTablePlugin"
+export {
+  default as WidthTablePlugin,
+  INSERT_WIDTH_TABLE_COMMAND,
+} from "./WidthTablePlugin"
