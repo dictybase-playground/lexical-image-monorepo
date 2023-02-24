dfca162642d1eaa8540556167fed1ca0980ac8b0
Kevin Tun <kevin.tun@northwestern.edu>
2023-02-24 12:29:01 -0600

    refactor(packages/image-plugin): change approach to moving dragged image node

--- packages/image-plugin/src/ImagePlugin.tsx
+++ packages/image-plugin/src/ImagePlugin.tsx
@@ -11,6 +11,8 @@ import {
   COMMAND_PRIORITY_HIGH,
   DROP_COMMAND,
   LexicalEditor,
+  $getNearestNodeFromDOMNode,
+  $createParagraphNode,
 } from "lexical"
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext"
 import ImageNode, { $isImageNode } from "./ImageNode"
@@ -34,82 +36,26 @@ const TRANSPARENT_IMAGE =
 const img = document.createElement("img")
 img.src = TRANSPARENT_IMAGE
 
-export const getImageNode = () => {
+export const getImageNodeFromSelection = () => {
   const selection = $getSelection()
   if (!$isNodeSelection(selection)) return null
   const nodes = selection.getNodes()
   return $isImageNode(nodes[0]) ? nodes[0] : null
 }
 
-export const getDragImageData = (event: DragEvent) => {
-  const dragData = event.dataTransfer?.getData("application/lexical-drag")
-  if (!dragData) return null
-  const { type, data } = JSON.parse(dragData)
-  if (type !== "image") return null
-
-  return data as InsertImagePayload
-}
-
-export const getDragSelection = (event: DragEvent) => {
-  let range
-
-  // Firefox?
-
-  // Other browsers
-  if (document.caretRangeFromPoint) {
-    range = document.caretRangeFromPoint(event.clientX, event.clientY)
-  }
-
-  return range
-}
-
 export const onDragStart = (event: DragEvent) => {
-  // 1: Get the ImageNode in the current editor selection
-  const imageNode = getImageNode()
-  if (!imageNode) {
-    return false
-  }
-  const { dataTransfer } = event
-  if (!dataTransfer) return false
-  // 2: Using event dataTransfer API, capture the properties of
-  //    the ImageNode in a JSON, including the node key.
-  //    When a drop event occurs, the JSON will be parsed and used to
-  //    create a new ImageNode
-  dataTransfer.setData(
-    "application/lexical-drag",
-    JSON.stringify({
-      data: {
-        source: imageNode.source,
-        alt: imageNode.alt,
-        width: imageNode.width,
-        height: imageNode.height,
-        key: imageNode.getKey(),
-      },
-      type: "image",
-    }),
-  )
-  dataTransfer.setDragImage(img, 0, 0)
+  event.dataTransfer?.setDragImage(img, 0, 0)
   return true
 }
 
-export const onDrop = (event: DragEvent, editor: LexicalEditor) => {
-  const node = getImageNode()
-  if (!node) return false
-
-  // 1: Get image data
-  const data = getDragImageData(event)
-  if (!data) return false
-
-  // 2: Get current selection range
-  const range = getDragSelection(event)
-  if (!range) return false
-  // 3: Remove previous ImageNode
-  node.remove()
-  // 4: Create new image node with image data at current selection
-  const rangeSelection = $createRangeSelection()
-  rangeSelection.applyDOMRange(range)
-  $setSelection(rangeSelection)
-  editor.dispatchCommand(INSERT_IMAGE_COMMAND, data)
+export const onDrop = (event: DragEvent) => {
+  if (!(event.target instanceof Node)) return false
+  const dropTargetNode = $getNearestNodeFromDOMNode(event.target)
+  if (!dropTargetNode) return false
+  const imageNode = getImageNodeFromSelection()
+  if (!imageNode) return false
+  imageNode.remove(false)
+  dropTargetNode.insertAfter(imageNode)
 
   return true
 }
