eadc1aa6020c40bad954c5cae98762616d2b71cf
Kevin Tun <kevin.tun@northwestern.edu>
2023-03-03 14:02:05 -0600

    chore(apps/dndkit-demo): remove unused code

--- apps/dndkit-demo/src/SortableItem.tsx
+++ apps/dndkit-demo/src/SortableItem.tsx
@@ -2,7 +2,6 @@ import { UniqueIdentifier } from "@dnd-kit/core"
 import { useSortable } from "@dnd-kit/sortable"
 import { CSS } from "@dnd-kit/utilities"
 import { Paper } from "@material-ui/core"
-import useSortableItemStyles from "./useSortableItemStyles"
 
 type SortableItemProperties = {
   id: UniqueIdentifier
@@ -12,11 +11,6 @@ const SortableItem = ({ id }: SortableItemProperties) => {
   const { setNodeRef, listeners, attributes, transform, transition } =
     useSortable({ id })
 
-  //  not sure why makeStyles isn't working correctly with it
-  // const { root } = useSortableItemStyles({
-  //   transform: CSS.Transform.toString(transform),
-  //   transition,
-  // })
   const style = {
     minHeight: "100px",
     marginBottom: "10px",
