5e87fc977dee368141f7364d37195089c440e797
Kevin Tun <kevin.tun@northwestern.edu>
2023-03-06 10:08:31 -0600

    refactor(apps/dndkit-demo): refactor onDragEndCreator

--- apps/dndkit-demo/src/dragHandlers.ts
+++ apps/dndkit-demo/src/dragHandlers.ts
@@ -1,17 +1,18 @@
-import { DragStartEvent, DragEndEvent, UniqueIdentifier } from "@dnd-kit/core"
+import { DragEndEvent, UniqueIdentifier } from "@dnd-kit/core"
 import { arrayMove } from "@dnd-kit/sortable"
 import { Dispatch, SetStateAction } from "react"
 
-export const onDragStart = (event: DragStartEvent) => {}
-export const onDragEndCreator =
+const onDragEndCreator =
   (setState: Dispatch<SetStateAction<UniqueIdentifier[]>>) =>
   (event: DragEndEvent) => {
     const { active, over } = event
+    if (!active.id || !over?.id) return
+
     setState((previousState) => {
-      if (!active || !over) return previousState
       const activeIndex = previousState.indexOf(active.id)
       const overIndex = previousState.indexOf(over.id)
-      console.log("setting state")
       return arrayMove(previousState, activeIndex, overIndex)
     })
   }
+
+export default onDragEndCreator
