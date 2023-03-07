2c0fcc7a9690e21857e2a77e9ad3adae4e1defc8
Kevin Tun <kevin.tun@northwestern.edu>
2023-03-07 11:50:36 -0600

    refactor(packages/image-plugin): use dataTransfer api to get dragged image data

--- packages/image-plugin/src/dragHandlers.ts
+++ packages/image-plugin/src/dragHandlers.ts
@@ -1,8 +1,9 @@
-import { $setSelection, LexicalEditor } from "lexical"
+import { $getNodeByKey, $setSelection, LexicalEditor } from "lexical"
 import {
-  getImageNodeFromSelection,
+  getImageNodeFromElement,
   getRangeSelectionFromPoint,
 } from "./dragHelpers"
+import { $isImageNode } from "./ImageNode"
 import { INSERT_IMAGE_COMMAND } from "./InsertImageCommand"
 // On Firefox, when the image is dragged, a transparent version of the image hovers
 // with the user's cursor. This can make it difficult to see where the image is
@@ -15,14 +16,21 @@ img.src = TRANSPARENT_IMAGE
 
 export const onDragStart = (event: DragEvent) => {
   event.dataTransfer?.setDragImage(img, 0, 0)
+  if (!(event.target instanceof Node)) return false
+  const imageNode = getImageNodeFromElement(event.target)
+  const imageNodeKey = imageNode?.getKey()
+  if (!imageNodeKey) return false
+  event.dataTransfer?.setData("text/lexical-key", imageNodeKey)
   return true
 }
 
 export const onDrop = (event: DragEvent, editor: LexicalEditor) => {
   event.preventDefault()
   if (!(event.target instanceof Node)) return false
-  const imageNode = getImageNodeFromSelection()
-  if (!imageNode) return false
+  const imageKey = event.dataTransfer?.getData("text/lexical-key")
+  if (!imageKey) return false
+  const imageNode = $getNodeByKey(imageKey)
+  if (!imageNode || !$isImageNode(imageNode)) return false
 
   $setSelection(getRangeSelectionFromPoint(event.clientX, event.clientY))
 
