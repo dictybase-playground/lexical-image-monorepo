6d6b17d4ce1c4aa2d0c793c7d597c1b1d6dc70b5
Kevin Tun <kevin.tun@northwestern.edu>
2023-03-13 09:25:55 -0500

    test(packages/table-action-plugin): write more tests for table actions

--- packages/table-action-plugin/src/tableActions.test.ts
+++ packages/table-action-plugin/src/tableActions.test.ts
@@ -1,18 +1,14 @@
 import { describe, test, expect, beforeEach } from "vitest"
-import {
-  LexicalEditor,
-  $getRoot,
-  createEditor,
-  NodeKey,
-  $getNodeByKey,
-} from "lexical"
+import { LexicalEditor, $getRoot, createEditor, NodeKey } from "lexical"
 import {
   TableNode,
   TableRowNode,
   TableCellNode,
   $createTableNodeWithDimensions,
+  $getTableRowNodeFromTableCellNodeOrThrow,
+  $getTableColumnIndexFromTableCellNode,
 } from "@lexical/table"
-import { deleteTable, insertRow } from "./tableActions"
+import { deleteTable, insertRow, insertColumn } from "./tableActions"
 
 const testConfig = {
   namespace: "testEditor",
@@ -29,6 +25,7 @@ beforeEach(() => {
     ...testConfig,
     nodes: [TableNode, TableRowNode, TableCellNode],
   })
+  testEditor.setRootElement(document.createElement("div"))
   testEditor.update(() => {
     tableNode = $createTableNodeWithDimensions(5, 5)
     tableNodeKey = tableNode.getKey()
@@ -58,17 +55,115 @@ describe("deleteTable", () => {
 
 describe("insertRow", () => {
   test("if insertAfter is true, the function will create a new row below the given table cell's row", () => {
-    let initialRowCount
+    let initialRowCount: unknown
     let finalRowCount
+    let initialTargetRowIndex: unknown
+    let finalTargetRowIndex: unknown
+    let targetRowKey: NodeKey
+    let newRowIndex
+    let initialRowKeyArray: NodeKey[]
+    let finalRowKeyArray
 
     testEditor.getEditorState().read(() => {
       initialRowCount = tableNode.getChildrenSize()
-    })
-    insertRow(testEditor, targetTableCell, { insertAfter: true })
-    testEditor.update(() => {
-      finalRowCount = $getNodeByKey(tableNodeKey).getChildrenSize()
+      const tableRows = tableNode.getChildren()
+      initialRowKeyArray = tableRows.map((row) => row.getKey())
+      targetRowKey =
+        $getTableRowNodeFromTableCellNodeOrThrow(targetTableCell).getKey()
+      initialTargetRowIndex = initialRowKeyArray.indexOf(targetRowKey)
     })
 
-    expect(finalRowCount).toEqual(initialRowCount + 1)
+    insertRow(testEditor, targetTableCell, { insertAfter: true })
+
+    testEditor.update(() => {
+      finalRowCount = tableNode.getChildrenSize()
+      const tableRows = tableNode.getChildren()
+      finalRowKeyArray = tableRows.map((row) => row.getKey())
+      const newRowKey = finalRowKeyArray.find(
+        (key) => !initialRowKeyArray.includes(key),
+      )
+      newRowIndex = newRowKey ? finalRowKeyArray.indexOf(newRowKey) : null
+      finalTargetRowIndex = finalRowKeyArray.indexOf(targetRowKey)
+    })
+    expect(finalRowCount).toEqual((initialRowCount as number) + 1)
+    expect(newRowIndex).toEqual((initialTargetRowIndex as number) + 1)
+    expect(finalTargetRowIndex).toEqual(initialTargetRowIndex)
   })
+
+  test("if insertAfter is false, the function will create a new row above the given table cell's row", () => {
+    let initialRowCount: unknown
+    let finalRowCount
+    let initialTargetRowIndex: unknown
+    let finalTargetRowIndex: unknown
+    let targetRowKey: NodeKey
+    let newRowIndex
+    let initialRowKeyArray: NodeKey[]
+    let finalRowKeyArray
+
+    testEditor.getEditorState().read(() => {
+      initialRowCount = tableNode.getChildrenSize()
+      const tableRows = tableNode.getChildren()
+      initialRowKeyArray = tableRows.map((row) => row.getKey())
+      targetRowKey =
+        $getTableRowNodeFromTableCellNodeOrThrow(targetTableCell).getKey()
+      initialTargetRowIndex = initialRowKeyArray.indexOf(targetRowKey)
+    })
+
+    insertRow(testEditor, targetTableCell, { insertAfter: false })
+
+    testEditor.update(() => {
+      finalRowCount = tableNode.getChildrenSize()
+      const tableRows = tableNode.getChildren()
+      finalRowKeyArray = tableRows.map((row) => row.getKey())
+      const newRowKey = finalRowKeyArray.find(
+        (key) => !initialRowKeyArray.includes(key),
+      )
+      newRowIndex = newRowKey ? finalRowKeyArray.indexOf(newRowKey) : null
+      finalTargetRowIndex = finalRowKeyArray.indexOf(targetRowKey)
+    })
+    expect(finalRowCount).toEqual((initialRowCount as number) + 1)
+    expect(newRowIndex).toEqual(initialTargetRowIndex)
+    expect(finalTargetRowIndex).toEqual((initialTargetRowIndex as number) + 1)
+  })
+})
+
+describe("insertColumn", () => {
+  test.todo(
+    "if insertAfter is false, the function will create a column before the given table cell's column",
+    () => {
+      let initialRowLengths: unknown
+      let finalRowLengths: unknown
+      let initialtargetColumnIndex
+      let newColumnCellIndex
+
+      testEditor.getEditorState().read(() => {
+        initialtargetColumnIndex =
+          $getTableColumnIndexFromTableCellNode(targetTableCell)
+
+        initialRowLengths =
+          $getTableRowNodeFromTableCellNodeOrThrow(
+            targetTableCell,
+          ).getChildrenSize()
+      })
+
+      insertColumn(testEditor, targetTableCell, { insertAfter: false })
+
+      testEditor.update(() => {
+        const tableCellKeyArray = tableNode
+          .getChildren()
+          .map((row) =>
+            row.getChildren().map((cell: TableCellNode) => cell.getKey()),
+          )
+        //   const new
+        // // newColumnIndex
+        finalRowLengths =
+          $getTableRowNodeFromTableCellNodeOrThrow(
+            targetTableCell,
+          ).getChildrenSize()
+      })
+
+      expect(finalRowLengths).toEqual((initialRowLengths as number) + 1)
+      expect(newColumnCellIndex).toEqual(initialtargetColumnIndex)
+    },
+  )
 })
