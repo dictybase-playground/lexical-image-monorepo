aad1a8dfd1272db3e74a3757730d0ac609ccc953
Kevin Tun <kevin.tun@northwestern.edu>
2023-03-27 06:00:02 -0500

    feat(packages/flex-layout-plugin): add logic to insert node before selected
    add logic to insert FlexLayoutNode before selected node and to carry
    text to new flexLayoutNode

--- packages/flex-layout-plugin/src/InsertFlexLayoutNode.ts
+++ packages/flex-layout-plugin/src/InsertFlexLayoutNode.ts
@@ -1,23 +1,75 @@
-import { $getSelection } from "lexical"
+import {
+  LexicalNode,
+  TextNode,
+  RangeSelection,
+  $isRootNode,
+  $getSelection,
+  $isRangeSelection,
+} from "lexical"
 import { $createFlexLayoutNode } from "./FlexLayoutNode"
 
-const getImmediateRootDescendantFromSelection = () => {
-  const selection = $getSelection()
-  let node = selection?.getNodes()[0]
-  while (node && node.getParent()?.getType() !== "root") {
-    node = node.getParentOrThrow()
-  }
-  return node
+const getImmediateRootDescendantFromLexicalNode = (
+  node: LexicalNode,
+): LexicalNode => {
+  const nodeParent = node.getParent()
+  if (!nodeParent) throw new Error("Node is not a descendant of a root node")
+  if ($isRootNode(nodeParent)) return node
+  return getImmediateRootDescendantFromLexicalNode(nodeParent)
 }
 
+const getImmediateRootDescendantFromRangeSelection = (
+  selection: RangeSelection,
+) => {
+  const node = selection.getNodes()[0]
+  return node ? getImmediateRootDescendantFromLexicalNode(node) : null
+}
+
+const getSelectionPoints = (range: RangeSelection) => {
+  const start = range.isBackward() ? range.focus : range.anchor
+  const end = range.isBackward() ? range.anchor : range.focus
+  return {
+    points: [start.offset, end.offset],
+    startNode: start.getNode(),
+    endNode: end.getNode(),
+  }
+}
+
+const splitText = (text: string, start: number, end: number) => [
+  text.slice(0, start),
+  text.slice(end),
+]
+
 const InsertFlexLayoutNode = () => {
-  const selectedNode = getImmediateRootDescendantFromSelection()
+  const selection = $getSelection()
+  if (!selection || !$isRangeSelection(selection)) return false
+  const selectedNode = getImmediateRootDescendantFromRangeSelection(selection)
   if (!selectedNode) return false
   const flexLayoutNode = $createFlexLayoutNode()
-  selectedNode.insertAfter(flexLayoutNode)
-  const paragraphNode = flexLayoutNode.getParagraphNodeOrThrow()
-  if (!paragraphNode) return false
-  paragraphNode.select()
+
+  const {
+    points: [start, end],
+    startNode,
+  } = getSelectionPoints(selection)
+
+  if (start === 0 || end === 0) {
+    selectedNode.insertBefore(flexLayoutNode)
+  } else {
+    selectedNode.insertAfter(flexLayoutNode)
+
+    const paragraphNode = flexLayoutNode.getParagraphNodeOrThrow()
+    if (!paragraphNode) return false
+    paragraphNode.select()
+
+    const [textToRemain, textToMove] = splitText(
+      selectedNode.getTextContent(),
+      start,
+      end,
+    )
+    if (textToMove) {
+      paragraphNode.append(new TextNode(textToMove))
+      startNode.setTextContent(textToRemain)
+    }
+  }
   return true
 }
 
