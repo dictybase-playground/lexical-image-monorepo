4311cad30c3b8905d234b424dd5ea61e28613392
Kevin Tun <kevin.tun@northwestern.edu>
2023-04-06 06:02:10 -0500

    * feat(flex-layout-plugin): add $isFlexLayoutNode export to FlexLayoutNode module

--- packages/flex-layout-plugin/src/index.ts
+++ packages/flex-layout-plugin/src/index.ts
@@ -1,4 +1,4 @@
-export { default as FlexLayoutNode } from "./FlexLayoutNode"
+export { default as FlexLayoutNode, $isFlexLayoutNode } from "./FlexLayoutNode"
 export { default as FlexLayoutDecoratorNode } from "./FlexLayoutDecoratorNode"
 export { default as FlexLayoutPlugin } from "./FlexLayoutPlugin"
 export { default as LexicalComposerFlex } from "./LexicalComposerFlex"
