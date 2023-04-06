e75b9a5c44974ab154c14a829ed5d5e9bf4271c5
Kevin Tun <kevin.tun@northwestern.edu>
2023-04-06 05:53:37 -0500

    feat(packages/flex-layout-plugin): add handleTextContent function

--- packages/flex-layout-plugin/src/helpers.ts
+++ packages/flex-layout-plugin/src/helpers.ts
@@ -1,5 +1,20 @@
-import { LexicalNode, RangeSelection, $isRootNode } from "lexical"
+import {
+  LexicalNode,
+  $isRootNode,
+  TextNode,
+  ParagraphNode,
+  RangeSelection,
+  NodeSelection,
+  GridSelection,
+} from "lexical"
+import { PointType } from "lexical/LexicalSelection"
 
+/**
+ * Retrieves the immediate root descendant from a given lexical node by recursively traversing up the lexical hierarchy until a root node is found. Throws an error if the node is not a descendant of a root node.
+ * @param node - The lexical node to find the immediate root descendant of.
+ * @throws An error is thrown if the given node is not a descendant of a root node.
+ * @returns The immediate root descendant of the given lexical node.
+ */
 const getImmediateRootDescendantFromLexicalNode = (
   node: LexicalNode,
 ): LexicalNode => {
@@ -9,25 +24,47 @@ const getImmediateRootDescendantFromLexicalNode = (
   return getImmediateRootDescendantFromLexicalNode(nodeParent)
 }
 
-export const getImmediateRootDescendantFromRangeSelection = (
-  selection: RangeSelection,
+/**
+ * Retrieves the immediate root descendant from the first node in a given selection.
+ * @param selection - The selection whose first node should be used to find its immediate root descendant.
+ * @returns The immediate root descendant of the first node in the given selection, or null if the selection has no nodes.
+ */
+export const getImmediateRootDescendant = (
+  selection: RangeSelection | NodeSelection | GridSelection,
 ) => {
   const node = selection.getNodes()[0]
   return node ? getImmediateRootDescendantFromLexicalNode(node) : null
 }
 
-export const getSelectionPoints = (range: RangeSelection) => {
-  const start = range.isBackward() ? range.focus : range.anchor
-  const end = range.isBackward() ? range.anchor : range.focus
-  return {
-    points: [start.offset, end.offset],
-    startNode: start.getNode(),
-    endNode: end.getNode(),
-  }
+export const getNodeAtCaret = (selection: RangeSelection) => {
+  if (!selection.isCollapsed()) return null
+  return selection.focus
 }
 
-export const getTextEdges = (
-  text: string,
-  offsetStart: number,
-  offsetEnd: number,
-) => [text.slice(0, offsetStart), text.slice(offsetEnd)]
+/**
+ * Retrieves an array containing two strings representing the substrings of a larger string split at a given offset.
+ * @param nodeText - The larger string to split into substrings.
+ * @param offset - The index within nodeText at which to perform the split.
+ * @returns An array containing two strings. The first represents nodeText up to the split point, and the second represents nodeText after the split point.
+ */
+const getTextEdges = (nodeText: string, offset: number) => [
+  nodeText.slice(0, offset),
+  nodeText.slice(offset),
+]
+
+/**
+ * Handles the text content of a given node by moving a specified portion of its text content to a new paragraph node.
+ * @param nodeWithText - The point type that represents the lexical node and offset at which to split the text content.
+ * @param paragraphNode - The paragraph node to append the new text node to.
+ */
+export const handleTextContent = (
+  nodeWithText: PointType,
+  paragraphNode: ParagraphNode,
+) => {
+  const [textToRemain, textToMove] = getTextEdges(
+    nodeWithText.getNode().getTextContent(),
+    nodeWithText.offset,
+  )
+  nodeWithText.getNode().setTextContent(textToRemain)
+  paragraphNode.append(new TextNode(textToMove))
+}
